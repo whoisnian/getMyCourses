cbb48db78a56adf9285bd1fa4e26002ca30f91f4
whoisnian <zhuchangbao1998@gmail.com>
2019-05-22 17:22:22 +0800

    :sparkles: add login via Northeastern University uniform identity authentication

--- login/login.go
+++ login/login.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"regexp"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -100,7 +102,108 @@ func LoginViaSupwisdom(username string, password string) (*cookiejar.Jar, error)
 
 // 统一身份认证：https://pass.neu.edu.cn
 func LoginViaTpass(username string, password string) (*cookiejar.Jar, error) {
-	fmt.Println("统一身份认证登录中。。。")
-	return nil, errors.New("LoginViaTpass 404")
-	//fmt.Println("统一身份登录认证登录完成。")
+	fmt.Println("\n统一身份认证登录中。。。")
+
+	// Cookie 自动维护
+	cookieJar, err := cookiejar.New(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// http 请求客户端
+	var client http.Client
+	client.Jar = cookieJar
+
+	// 第一次请求
+	req, err := http.NewRequest(http.MethodGet, "http://219.216.96.4/eams/localLogin!tip.action", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// 发送
+	resp1, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp1.Body.Close()
+
+	// 第二次请求
+	req, err = http.NewRequest(http.MethodGet, "https://pass.neu.edu.cn/tpass/login?service=http%3A%2F%2F219.216.96.4%2Feams%2FhomeExt.action", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// 发送
+	resp2, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp2.Body.Close()
+
+	// 读取
+	content, err := ioutil.ReadAll(resp2.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// 检查
+	temp := string(content)
+	if !strings.Contains(temp, "<form id=\"loginForm\" action=\"/tpass/login") {
+		return nil, errors.New("登录页面打开失败，请检查 https://pass.neu.edu.cn")
+	}
+
+	// 提取表单信息
+	reg1 := regexp.MustCompile(`id="loginForm" action="(/tpass/login;jsessionid=[^"]*)`)
+	form_action := "https://pass.neu.edu.cn" + reg1.FindStringSubmatch(temp)[1]
+	reg2 := regexp.MustCompile(`id="lt" name="lt" value="([^"]*)`)
+	form_lt := reg2.FindStringSubmatch(temp)[1]
+	form_rsa := username + password + form_lt
+	form_ul := len(username)
+	form_pl := len(password)
+	reg3 := regexp.MustCompile(`name="execution" value="([^"]*)`)
+	form_execution := reg3.FindStringSubmatch(temp)[1]
+	reg4 := regexp.MustCompile(`name="_eventId" value="([^"]*)`)
+	form__eventId := reg4.FindStringSubmatch(temp)[1]
+
+	// 第三次请求
+	time.Sleep(1 * time.Second)
+	formValues := make(url.Values)
+	formValues.Set("rsa", form_rsa)
+	formValues.Set("ul", strconv.Itoa(form_ul))
+	formValues.Set("pl", strconv.Itoa(form_pl))
+	formValues.Set("lt", form_lt)
+	formValues.Set("execution", form_execution)
+	formValues.Set("_eventId", form__eventId)
+
+	req, err = http.NewRequest(http.MethodPost, form_action, strings.NewReader(formValues.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0")
+
+	// 发送
+	resp3, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp3.Body.Close()
+
+	// 读取
+	content, err = ioutil.ReadAll(resp3.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// 检查
+	temp = string(content)
+	if !strings.Contains(temp, "personal-name") {
+		return nil, errors.New("登录失败，请检查用户名和密码")
+	}
+
+	temp = temp[strings.Index(temp, "class=\"personal-name\">")+23 : strings.Index(temp, "class=\"personal-name\">")+60]
+	fmt.Println(temp[:strings.Index(temp, ")")+1])
+
+	fmt.Println("统一身份登录认证登录完成。")
+	return cookieJar, nil
 }
